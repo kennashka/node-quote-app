0304cd4c9daa60757d713dff94b6aef1e2fda506
kennashka <kennashka@yahoo.com>
2018-10-26 18:36:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
+app.use( express.static( "public" ) );
+
+// index page
+app.get('/', function(req, res) {
+    res.render('page/index');
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Qoute app listening on port 3000!');
+});
